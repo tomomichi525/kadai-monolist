fd9e86662e041e8103c2ca4698b5b3878ce5677a
tomomi.kashiwagi <tomo525@hotmail.com>
2018-06-26 03:40:09 +0000

    rakuten api

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ Route::get('/', 'WelcomeController@index');
 // User registration
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// Login authentication
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('items', 'ItemsController', ['only' => ['create']]);
+});
